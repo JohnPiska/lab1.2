2574a98b334f3e9a685d2014f5e688eefc7e47b8
Yusuf Belyaev <byusuf66@yandex.ru>
2025-02-26 20:50:46 +0500

    Добавьте файлы проекта.

---
+++ lab1.2/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let rec productOfDigits n =
+    if n < 10 then n               
+    else (n % 10) * productOfDigits (n / 10)
+
+
+printf "Введите натуральное число: "
+let number = Console.ReadLine() |> int
+let result = productOfDigits number
+    
+printfn "Произведение цифр: %d" result
+
